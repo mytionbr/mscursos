c06865336e300ba0a888dba6d1c2b6c0ec72093c
mytionBr <matheuss.jesus2017@gmail.com>
2021-08-02 10:39:41 -0300

    adicionar componentes de total alunos e total aulas

--- client/src/components/professor/dashboard/TotalAlunos/TotalAlunos.jsx
+++ client/src/components/professor/dashboard/TotalAlunos/TotalAlunos.jsx
@@ -1,10 +1,47 @@
+import { Avatar, Card, CardContent, Grid, Typography } from '@material-ui/core'
 import React from 'react'
-
-function TotalAlunos() {
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+import { green } from '@material-ui/core/colors';
+function TotalAlunos(props) {
     return (
-        <div>
-            
-        </div>
+        <Card
+            sx={{height: '100%'}}
+            {...props}
+        >
+            <CardContent>
+                <Grid
+                    container
+                    spacing={3}
+                    sx={{justifyContent: 'space-between'}}
+                >
+                    <Typography
+                        color="textSecondary"
+                        gutterBottom
+                        variant="h3"
+                    >
+                        Minhas Aulas
+                    </Typography>
+                    <Typography
+                        color="textPrimary"
+                        variant="h3"
+                    >
+                        12
+                    </Typography>
+                
+                    <Grid item>
+                        <Avatar
+                            sx={{
+                                backgroundColor: green[600],
+                                height: 56,
+                                width: 56
+                            }}
+                        >
+                            <PeopleOutlineIcon />
+                        </Avatar>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
     )
 }
 
--- client/src/components/professor/dashboard/TotalAulas/TotalAulas.jsx
+++ client/src/components/professor/dashboard/TotalAulas/TotalAulas.jsx
@@ -1,10 +1,48 @@
+import { Avatar, Card, CardContent, Grid, Typography } from '@material-ui/core'
+import { red } from '@material-ui/core/colors'
 import React from 'react'
+import ClassIcon from '@material-ui/icons/Class';
 
-function TotalAulas() {
+function TotalAulas(props) {
     return (
-        <div>
-            
-        </div>
+        <Card
+            sx={{height: '100%'}}
+            {...props}
+        >
+            <CardContent>
+                <Grid
+                    container
+                    spacing={3}
+                    sx={{justifyContent: 'space-between'}}
+                >
+                    <Typography
+                        color="textSecondary"
+                        gutterBottom
+                        variant="h3"
+                    >
+                        Minhas Aulas
+                    </Typography>
+                    <Typography
+                        color="textPrimary"
+                        variant="h3"
+                    >
+                        12
+                    </Typography>
+                
+                    <Grid item>
+                        <Avatar
+                            sx={{
+                                backgroundColor: red[600],
+                                height: 56,
+                                width: 56
+                            }}
+                        >
+                            <ClassIcon />
+                        </Avatar>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
     )
 }
 
