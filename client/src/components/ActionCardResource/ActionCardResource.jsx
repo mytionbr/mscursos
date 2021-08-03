c076f6c40533a23f03cf95999bb297ba4e5cde3c
mytionBr <matheuss.jesus2017@gmail.com>
2021-08-03 01:21:38 -0300

    adicionar efeito de click no ActionCardResoucer

--- client/src/components/ActionCardResource/ActionCardResource.jsx
+++ client/src/components/ActionCardResource/ActionCardResource.jsx
@@ -1,60 +1,64 @@
-import { Avatar, Box, Card, CardContent, Typography } from '@material-ui/core'
-import React from 'react'
-import AddIcon from '@material-ui/icons/Add';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import AddIcon from "@material-ui/icons/Add";
 
-function ActionCardResource({resource, ...rest}) {
-    return (
-       <Card
-        style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100%'
-        }}
-        {...rest}
-       >
-           <CardContent>
-                <Box
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        padding:'1rem'
-                    }}
-                >
-                    <Avatar
-                         style={{
-                            background: '#fff',
-                            height: '10rem',
-                            width:'8rem',
-                        }}                 
-                    >
-                        <AddIcon
-                             style={{
-                                color:'#757575',
-                                height: '100%',
-                                width:'100%',
-                            }}   
-                        />
-                    </Avatar>
-                </Box>
-                <Typography
-                    align="center"
-                    color="textPrimary"
-                    gutterBottom
-                    variant="h4"
-                >
-                    {resource.name}
-                </Typography>
-                <Typography
-                    align="center"
-                    color="textPrimary"
-                    variant="body1"
-                >
-                    {resource.description}
-                </Typography>
-           </CardContent>
-
-       </Card>
-    )
+function ActionCardResource({ resource, ...rest }) {
+  return (
+    <Card
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+      }}
+      {...rest}
+    >
+      <CardActionArea>
+        <CardContent>
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "1rem",
+            }}
+          >
+            <Avatar
+              style={{
+                background: "#fff",
+                height: "10rem",
+                width: "8rem",
+              }}
+            >
+              <AddIcon
+                style={{
+                  color: "#757575",
+                  height: "100%",
+                  width: "100%",
+                }}
+              />
+            </Avatar>
+          </Box>
+          <Typography
+            align="center"
+            color="textPrimary"
+            gutterBottom
+            variant="h4"
+          >
+            {resource.name}
+          </Typography>
+          <Typography align="center" color="textPrimary" variant="body1">
+            {resource.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
 
-export default ActionCardResource
+export default ActionCardResource;
