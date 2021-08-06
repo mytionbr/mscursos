16ebb90041cf4a4153bcf72f1f8238cd2f25ed8b
mytionBr <matheuss.jesus2017@gmail.com>
2021-08-06 13:16:58 -0300

    mudar o escopo dos dados e funções de componente

--- client/src/components/professor/cursoList/CursoListResults/CursoListResults.jsx
+++ client/src/components/professor/cursoList/CursoListResults/CursoListResults.jsx
@@ -1,103 +1,102 @@
-import { Box, Card, Table, TableBody, TableCell, TableHead, TablePagination, TableRow, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
-import PerfectScrollbar  from 'react-perfect-scrollbar'
-import MenuButton from '../MenuButton/MenuButton'
+import {
+  Box,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import { useDispatch, useSelector } from "react-redux";
+import { findCursosByProfessor } from "../../../../actions/cursoActions";
+import LoadingBox from "../../../core/LoadingBox/LoadingBox";
+import MessageBox from "../../../core/MessageBox/MessageBox";
+import MenuButton from "../MenuButton/MenuButton";
 
-function CursoListResults({cursos, ...rest}) {
-    const [limit, setLimit] = useState(10)
-    const [page,setPage] = useState(0)
-    
+function CursoListResults(props) {
+  const dispatch = useDispatch();
+  const cursoProfessor = useSelector((state) => state.cursoProfessor);
+  const { loading, error, data } = cursoProfessor;
+  const cursos = data ? data : [];
 
-    const handleLimitChange = (event) =>{
-        setLimit(event.target.value)
-    }
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
 
-    const handlePageChange = (event, newPage) => {
-        setPage(newPage)
-    }
+  useEffect(() => {
+    dispatch(findCursosByProfessor());
+  }, [dispatch]);
 
-    return (
-        <Card {...rest}>
-            <PerfectScrollbar component="div">
-                <Box style={{minWidth: '1050px'}}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>
-                                    Id
-                                </TableCell>
-                                <TableCell>
-                                    Nome
-                                </TableCell>
-                                <TableCell>
-                                    Descrição
-                                </TableCell>
-                                <TableCell>
-                                    categoria
-                                </TableCell>
-                                <TableCell>
-                                    Aulas
-                                </TableCell>
-                                <TableCell>
-                                    Alunos
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {cursos.slice(page * limit, page * limit + limit).map((curso) =>(
-                                <TableRow
-                                    hover
-                                    key={curso.curso_id}
-                                >
-                                    <TableCell >
-                                        {curso.curso_id}
-                                    </TableCell>
-                                    <TableCell>
-                                        <Typography
-                                            color="textPrimary"
-                                            variant="body1"
-                                        >
-                                            {curso.nome}
-                                        </Typography>
-                                    </TableCell>
-                                    <TableCell>
-                                        {curso.descricao}
-                                    </TableCell>
-                                    <TableCell>
-                                        {curso.categoria_nome}
-                                    </TableCell>
-                                    <TableCell>
-                                        {curso.aulas}
-                                    </TableCell>
-                                    <TableCell>
-                                        {curso.alunos}
-                                    </TableCell>
-                                    <TableCell>
-                                        {curso.alunos}
-                                    </TableCell>
-                                    <TableCell>
-                                        <MenuButton 
-                                            id={curso.curso_id}
-                                        />
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </Box>
-            </PerfectScrollbar>
-            <TablePagination 
-                component="div"
-                count={cursos.length}
-                onPageChange={handlePageChange}
-                onRowsPerPageChange={handleLimitChange}
-                page={page}
-                rowsPerPage={limit}
-                rowsPerPageOptions={[5,10,25]}
-            />
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
 
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  return (
+    <>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox type="error">{error}</MessageBox>
+      ) : (
+        <Card {...props}>
+          <PerfectScrollbar component="div">
+            <Box style={{ minWidth: "1050px" }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Nome</TableCell>
+                    <TableCell>Descrição</TableCell>
+                    <TableCell>categoria</TableCell>
+                    <TableCell>Aulas</TableCell>
+                    <TableCell>Alunos</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {cursos
+                    .slice(page * limit, page * limit + limit)
+                    .map((curso) => (
+                      <TableRow hover key={curso.curso_id}>
+                        <TableCell>{curso.curso_id}</TableCell>
+                        <TableCell>
+                          <Typography color="textPrimary" variant="body1">
+                            {curso.nome}
+                          </Typography>
+                        </TableCell>
+                        <TableCell>{curso.descricao}</TableCell>
+                        <TableCell>{curso.categoria_nome}</TableCell>
+                        <TableCell>{curso.aulas}</TableCell>
+                        <TableCell>{curso.alunos}</TableCell>
+                        <TableCell>{curso.alunos}</TableCell>
+                        <TableCell>
+                          <MenuButton id={curso.curso_id} />
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </PerfectScrollbar>
+          <TablePagination
+            component="div"
+            count={cursos.length}
+            onPageChange={handlePageChange}
+            onRowsPerPageChange={handleLimitChange}
+            page={page}
+            rowsPerPage={limit}
+            rowsPerPageOptions={[5, 10, 25]}
+          />
         </Card>
-    )
+      )}
+    </>
+  );
 }
 
-export default CursoListResults
+export default CursoListResults;
--- client/src/screens/Professor/CursoList/CursoList.jsx
+++ client/src/screens/Professor/CursoList/CursoList.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Box, Container } from "@material-ui/core";
 import { Helmet } from "react-helmet";
-import CursoListToobar from "../../../components/professor/cursoList/CursoListToobar/CursoListToolbar";
 import CursoListResults from "../../../components/professor/cursoList/CursoListResults/CursoListResults";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  findCursos,
-  findCursosByProfessor,
-} from "../../../actions/cursoActions";
+
 import LoadingBox from "../../../components/core/LoadingBox/LoadingBox";
 import MessageBox from "../../../components/core/MessageBox/MessageBox";
 import ModalFilter from "../../../components/professor/cursoList/ModalFilter/ModalFilter";
@@ -16,20 +12,13 @@ import ToolbarPage from "../../../components/professor/ToolbarPage/ToolbarPage";
 
 function CursoList() {
   const [openModal, setOpenModal] = useState(false);
-
   const history = useHistory();
-  const dispatch = useDispatch();
-  const cursoProfessor = useSelector((state) => state.cursoProfessor);
-  const { loading, error, data } = cursoProfessor;
-  const cursos = data ? data : [];
 
   const handleOpenModal = () => {
     setOpenModal(!openModal);
   };
 
-  useEffect(() => {
-    dispatch(findCursosByProfessor());
-  }, [dispatch]);
+ 
 
   const links = [
     {
@@ -45,11 +34,11 @@ function CursoList() {
   const btns = [
     {
       name: "Criar novo Curso",
-      action: ()=> history.push('/professor/app/cursos/novo')
+      action: () => history.push("/professor/app/cursos/novo"),
     },
     {
       name: "Filtrar",
-      action:  handleOpenModal
+      action: handleOpenModal,
     },
   ];
 
@@ -69,13 +58,7 @@ function CursoList() {
           <ToolbarPage title={"Cursos"} links={links} btns={btns} />
 
           <Box style={{ padding: "3rem 0" }}>
-            {loading ? (
-              <LoadingBox />
-            ) : error ? (
-              <MessageBox type="error">{error}</MessageBox>
-            ) : (
-              <CursoListResults cursos={cursos} />
-            )}
+            <CursoListResults />
           </Box>
         </Container>
       </Box>
