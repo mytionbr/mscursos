555b68036c6c7e871a59f2cb2774cacc56b15352
mytionBr <matheuss.jesus2017@gmail.com>
2021-08-18 17:32:27 -0300

    adicionar AlunoRoute

---
+++ client/src/components/aluno/AlunoRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Redirect, Route } from 'react-router-dom'
+
+function AlunoRoute({component: Component, ...rest}) {
+    const alunoSignin = useSelector((state) => state.alunoSignin)
+    const { alunoInfo } = alunoSignin
+
+    return (
+        <Route
+            {...rest}
+            render={(props) =>
+                alunoInfo  ? (
+                    <Component {...props}></Component>
+                   
+                ) : (
+                    <Redirect to="/aluno/signin"/>
+                ) 
+            }
+        ></Route>
+    )
+}
+
+export default AlunoRoute
--- client/src/routers/AlunoRouter.jsx
+++ client/src/routers/AlunoRouter.jsx
@@ -5,13 +5,25 @@ import AlunoPlans from '../screens/Aluno/AlunoPlans/AlunoPlans'
 import  Home  from '../screens/Aluno/Home'
 import RegisterInfoMatricula from '../screens/Aluno/Matricula/RegisterInfoMatricula'
 import SigninAluno from '../screens/Aluno/SigninAluno/SigninAluno'
-
+import DashboardLayout from '../components/aluno/dashboard/DashboardLayout/DashboardLayout'
+import AlunoRoute from '../components/aluno/AlunoRoute'
 const AlunoRouter = () => {
+    
+    const routes = () => {
+        return (
+          <DashboardLayout>
+            <Switch>
+                <div>Oi</div>
+            </Switch>
+          </DashboardLayout>
+        );
+      };
+
     return(
         <div>
             <Navbar />
             <Switch> 
-                
+                <AlunoRoute path={"/aluno/app"} component={routes} />
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/aluno/signin" component={SigninAluno}/>
                 <Route exact path="/aluno/matriculas" component={AlunoPlans}/>
