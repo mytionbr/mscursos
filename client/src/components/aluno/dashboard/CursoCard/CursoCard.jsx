a3f2c3c994c7bf5b357a7fa86425047040fcff1c
mytionBr <matheuss.jesus2017@gmail.com>
2021-09-04 14:10:16 -0300

    adicionar CardActionArea no CursoCard

--- client/src/components/aluno/dashboard/CursoCard/CursoCard.jsx
+++ client/src/components/aluno/dashboard/CursoCard/CursoCard.jsx
@@ -1,6 +1,7 @@
 import {
   Avatar,
   Card,
+  CardActionArea,
   CardContent,
   Fade,
   Grid,
@@ -17,65 +18,88 @@ import ProgressBar from "../../../ProgressBar/ProgressBar";
 function CursoCard(props) {
   const { name, percent, size, categoriaId, slug } = props;
   const history = useHistory();
-  
-  const action = ()=>{
-    history.push(`/aluno/app/curso/${slug}`)
-  }
 
-  const handleSize = (size)=>{
-    switch(size){
-      case 'primary':
-        return {name: 'h4', avatar:{height: '6.5rem', width: '6.5rem', svg: '3rem'}}
-      case 'secondary':
-        return {name: 'h5',avatar:{height: '6rem', width: '6rem', svg: '3rem'}}
-        case 'tertiary':
-          return {name: 'h6',avatar:{height: '5rem', width: '5rem', svg: '1rem'}}
+  const action = () => {
+    history.push(`/aluno/app/curso/${slug}`);
+  };
+
+  const handleSize = (size) => {
+    switch (size) {
+      case "primary":
+        return {
+          name: "h4",
+          avatar: { height: "6.5rem", width: "6.5rem", svg: "3rem" },
+        };
+      case "secondary":
+        return {
+          name: "h5",
+          avatar: { height: "6rem", width: "6rem", svg: "3rem" },
+        };
+      case "tertiary":
+        return {
+          name: "h6",
+          avatar: { height: "5rem", width: "5rem", svg: "1rem" },
+        };
       default:
-        return {name: 'h4', avatar:{height: '6rem', width: '6rem', svg: '3rem'}}
+        return {
+          name: "h4",
+          avatar: { height: "6rem", width: "6rem", svg: "3rem" },
+        };
     }
-  }
+  };
 
-  const cardSize = handleSize(size)
+  const cardSize = handleSize(size);
   const classes = useStyles();
 
-  const Icon = getIconByCategoria(categoriaId)
-  
+  const Icon = getIconByCategoria(categoriaId);
+
   return (
     <Fade appear={true} in={true} timeout={700}>
-    <Card 
-      className={classes.card} 
-      onClick={action}
-      {...props}>
-      <CardContent>
-        <Grid container className={classes.gridContainer}>
-          <Grid item className={classes.gridHeader} xs={4}>
-            <Avatar 
-              className={classes.avatar}
-              style={{
-                height: cardSize.avatar.height,
-                width: cardSize.avatar.width,
-                "& > svg ": {
-                  fontSize: cardSize.avatar.svg,
-                },
-              }}>{ <Icon />}</Avatar>
-          </Grid>
-        
-        <Grid item xs={8} className={classes.gridPercent}>
-        <Typography align={'right'}  gutterBottom variant={cardSize.name}>
-              {name}
-        </Typography>
-          <Typography align={'right'} color="textSecondary" gutterBottom variant="h5">
-            {percent}%
-          </Typography>
-          <ProgressBar
-             percent={percent}
-             color={'#1a90ff'}
-             className={classes.percent}
-          />
-        </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+      <Card className={classes.card} onClick={action} {...props}>
+        <CardActionArea>
+          <CardContent>
+            <Grid container className={classes.gridContainer}>
+              <Grid item className={classes.gridHeader} xs={4}>
+                <Avatar
+                  className={classes.avatar}
+                  style={{
+                    height: cardSize.avatar.height,
+                    width: cardSize.avatar.width,
+                    "& > svg ": {
+                      fontSize: cardSize.avatar.svg,
+                    },
+                  }}
+                >
+                  {<Icon />}
+                </Avatar>
+              </Grid>
+
+              <Grid item xs={8} className={classes.gridPercent}>
+                <Typography
+                  align={"right"}
+                  gutterBottom
+                  variant={cardSize.name}
+                >
+                  {name}
+                </Typography>
+                <Typography
+                  align={"right"}
+                  color="textSecondary"
+                  gutterBottom
+                  variant="h5"
+                >
+                  {percent}%
+                </Typography>
+                <ProgressBar
+                  percent={percent}
+                  color={"#1a90ff"}
+                  className={classes.percent}
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Fade>
   );
 }
