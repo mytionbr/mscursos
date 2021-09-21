36baa4f8d98f2f88d573b35d937086d5aee7e9b6
mytionBr <matheuss.jesus2017@gmail.com>
2021-09-21 05:28:05 -0300

    test

--- server/app.js
+++ server/app.js
@@ -35,9 +35,9 @@ const __dirname = path.resolve()
 
 console.log(__dirname)
 
-app.use(express.static(path.join(__dirname, '/mscursos/client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/mscursos/client/build/index.html'))
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
 );
 
 export default app
