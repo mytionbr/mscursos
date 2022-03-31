20d088f85e3be91e583ab00d8ba20a85bc39b792
matheus jesus <matheuss.jesus2017@gmail.com>
2022-03-31 13:37:47 -0300

    fix environment variable name

--- server/config/config.js
+++ server/config/config.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const dbDetails ={
-                    host: process.env.LOCAL_HOST,
-                    user: process.env.USER,
+                    host: process.env.HOST,
+                    user: process.env.DATABASE_USER,
                     database: process.env.DATABASE,
                     password: process.env.PASSWORD,
                     port: process.env.DATABASE_PORT,
